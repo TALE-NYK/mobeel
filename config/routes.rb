02d5cd2c59610da28dc7a72503d24bbcd3ce79d1
masaya oda <tale@TALE-PC.lan>
2025-02-12 22:05:16 +0900

    ルーティングを修正、DB定義を実施

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
-Rails.application.routes.draw do
-  resources :tasks
-  resources :goals
+Rails.application.routes.draw do # rubocop:disable Style/FrozenStringLiteralComment
+  resources :goals, only: %i[index new create update edit show destroy] do
+    resources :tasks, only: %i[new create edit update destroy]
+  end
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
@@ -14,4 +15,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get '/auth/:provider/callback', as: :login
+  get '/', as: :index
 end
